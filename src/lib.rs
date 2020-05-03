8006c77162edf69dd17bfe859d3f0eefe6dfe0ee
theseriousadult <jack@gallabytes.com>
2020-05-03 01:56:26 -0700

    slab-y rewrite
    this should make a lot of other changes easier, but it was a bit painful

--- src/lib.rs
+++ src/lib.rs
@@ -1,148 +1,283 @@
-type Slot<T> = Option<Box<Node<T>>>;
+#![allow(dead_code)]
 
-pub struct Node<T> {
-    prefix: Vec<u8>,
-    val: Option<T>,
-    items: [Slot<T>; 256],
+use slab::Slab;
+use smallvec::SmallVec;
+use std::num::NonZeroUsize;
+
+pub struct Trie<T> {
+    node_slab: Slab<Node>,
+    item_slab: Slab<T>,
+    root: Node,
 }
 
-impl<T> Node<T> {
+mod keys {
+    use super::*;
+    #[derive(Clone, Copy, Debug)]
+    pub(super) struct ItemKey(NonZeroUsize);
+
+    impl ItemKey {
+        fn as_usize(self) -> usize {
+            self.0.get() - 1
+        }
+
+        pub(super) fn insert<T>(slab: &mut Slab<T>, val: T) -> Self {
+            let raw = slab.insert(val);
+            let key = unsafe { NonZeroUsize::new_unchecked(raw + 1) };
+            Self(key)
+        }
+
+        pub(super) fn get<T>(self, slab: &Slab<T>) -> &T {
+            slab.get(self.as_usize()).unwrap()
+        }
+
+        pub(super) fn get_mut<T>(self, slab: &mut Slab<T>) -> &mut T {
+            slab.get_mut(self.as_usize()).unwrap()
+        }
+
+        pub(super) fn remove<T>(self, slab: &mut Slab<T>) -> T {
+            slab.remove(self.as_usize())
+        }
+    }
+
+    #[derive(Clone, Copy, Debug)]
+    pub(super) struct NodeKey(NonZeroUsize);
+
+    impl NodeKey {
+        fn as_usize(self) -> usize {
+            self.0.get() - 1
+        }
+
+        pub(super) fn insert(slab: &mut Slab<Node>, val: Node) -> Self {
+            let raw = slab.insert(val);
+            NodeKey(unsafe { NonZeroUsize::new_unchecked(raw + 1) })
+        }
+
+        pub(super) fn get(self, slab: &Slab<Node>) -> &Node {
+            slab.get(self.as_usize()).unwrap()
+        }
+
+        pub(super) fn get_mut(self, slab: &mut Slab<Node>) -> &mut Node {
+            slab.get_mut(self.as_usize()).unwrap()
+        }
+
+        pub(super) fn remove(self, slab: &mut Slab<Node>) -> Node {
+            slab.remove(self.as_usize())
+        }
+    }
+}
+
+use keys::*;
+
+#[derive(Clone)]
+struct Node {
+    prefix: SmallVec<[u8; 8]>,
+    val: Option<ItemKey>,
+    slots: [Option<NodeKey>; 256],
+}
+
+impl Node {
     fn new() -> Self {
-        // we have to use uninit here because no const generics :(
-        use std::mem::MaybeUninit;
-
-        let mut items: [MaybeUninit<Slot<T>>; 256] = unsafe { MaybeUninit::uninit().assume_init() };
-        for item in items.iter_mut() {
-            *item = MaybeUninit::new(None)
-        }
-
         Node {
-            prefix: Vec::new(),
+            prefix: SmallVec::new(),
             val: None,
-            items: unsafe { std::mem::transmute(items) },
+            slots: [None; 256],
         }
     }
 
-    pub fn get<'a>(&'a self, key: &[u8]) -> Option<&'a T> {
-        let WalkTo { curr, shared, rest } = self.walk_to(key);
-
-        if rest == b"" && shared.len() == curr.prefix.len() {
-            curr.val.as_ref()
-        } else {
-            None
-        }
+    fn is_leaf(&self) -> bool {
+        self.slots.iter().all(|o| o.is_none())
     }
 
-    pub fn get_mut<'a>(&'a mut self, key: &[u8]) -> Option<&'a mut T> {
-        let WalkToMut { curr, shared, rest } = self.walk_to_mut(key);
-
-        if rest == b"" && shared.len() == curr.prefix.len() {
-            curr.val.as_mut()
-        } else {
-            None
-        }
-    }
-
-    pub fn insert(&mut self, key: &[u8], val: T) -> Option<T> {
-        let WalkToMut { curr, shared, rest } = self.walk_to_mut(key);
-
-        if shared.len() == curr.prefix.len() {
-            // no need to split
-            if rest.is_empty() {
-                // insert here
-                curr.val.replace(val)
-            } else {
-                // insert 1 down
-                let (d, suff) = rest.split_first().unwrap();
-                let mut new = Self::new();
-
-                new.prefix = suff.to_vec();
-                new.val.replace(val);
-
-                curr.items[*d as usize].replace(Box::new(new));
-
-                None
-            }
-        } else {
-            debug_assert!(shared.len() < curr.prefix.len());
-
-            let mut new_curr = Self::new();
-            new_curr.prefix = shared.to_vec();
-
-            if rest.is_empty() {
-                new_curr.val.replace(val);
-            } else {
-                let mut new_loc = Self::new();
-                new_loc.prefix = rest[1..].to_vec();
-                new_loc.val.replace(val);
-
-                new_curr.items[rest[0] as usize].replace(Box::new(new_loc));
-            }
-
-            // next we swap new_curr into place
-            std::mem::swap(curr, &mut new_curr);
-            let mut old_curr = new_curr;
-
-            // and finally we put old_curr in its rightful location under new_curr
-            let d = old_curr.prefix[shared.len()];
-            let suff = old_curr.prefix[shared.len() + 1..].to_vec();
-            old_curr.prefix = suff;
-            curr.items[d as usize].replace(Box::new(old_curr));
-
-            None
-        }
+    fn is_empty(&self) -> bool {
+        self.val.is_none() && self.is_leaf()
     }
 }
 
-macro_rules! walk_to_body {
-    ($this: expr, $key: expr, $ctor: tt,  $as_deref: tt) => {
-        let mut curr = $this;
-        let mut rest = $key;
+struct BreadCrumb<'a> {
+    node: &'a Node,
+    branch: u8,
+}
+
+struct WalkTo<'a, 'b> {
+    curr: &'a Node,
+    trail: Vec<BreadCrumb<'a>>,
+    shared: &'b [u8],
+    rest: &'b [u8],
+}
+
+impl<'a, 'b> WalkTo<'a, 'b> {
+    fn last_key(&self) -> Option<NodeKey> {
+        let last = self.trail.last()?;
+        last.node.slots[last.branch as usize]
+    }
+}
+
+impl<T> Trie<T> {
+    #[inline]
+    fn walk_to<'a, 'b>(&'a self, key: &'b [u8]) -> WalkTo<'a, 'b> {
+        let mut trail = Vec::with_capacity(key.len());
+        let mut curr = &self.root;
+        let mut rest = key;
 
         loop {
             let pref = curr.prefix.as_slice();
             let shared_len = shared_len(pref, rest);
             let (shared, suff) = rest.split_at(shared_len);
 
-            if !suff.is_empty() && pref == shared && curr.items[suff[0] as usize].is_some() {
-                // traverse
-                let next = curr.items[suff[0] as usize].$as_deref().unwrap();
-                curr = next;
-                rest = &suff[1..];
-            } else {
-                // dead end
-                return $ctor {
-                    curr,
-                    shared,
-                    rest: suff,
-                };
+            if !suff.is_empty() && pref.len() == shared.len() {
+                debug_assert!(pref == shared);
+
+                if let Some(ix) = curr.slots[suff[0] as usize] {
+                    // recurse
+                    trail.push(BreadCrumb {
+                        node: curr,
+                        branch: suff[0],
+                    });
+                    curr = ix.get(&self.node_slab);
+                    rest = &suff[1..];
+                    continue;
+                }
             }
+
+            // dead end
+            return WalkTo {
+                curr,
+                trail,
+                shared,
+                rest,
+            };
         }
-    };
-}
-
-impl<T> Node<T> {
-    #[inline]
-    fn walk_to<'a, 'b>(&'a self, key: &'b [u8]) -> WalkTo<'a, 'b, T> {
-        walk_to_body!(self, key, WalkTo, as_deref);
     }
 
-    #[inline]
-    fn walk_to_mut<'a, 'b>(&'a mut self, key: &'b [u8]) -> WalkToMut<'a, 'b, T> {
-        walk_to_body!(self, key, WalkToMut, as_deref_mut);
+    pub fn get<'a>(&'a self, key: &[u8]) -> Option<&'a T> {
+        let WalkTo {
+            shared, rest, curr, ..
+        } = self.walk_to(key);
+
+        if rest.is_empty() && shared.len() == curr.prefix.len() {
+            let ix = curr.val?;
+            Some(ix.get(&self.item_slab))
+        } else {
+            None
+        }
     }
-}
 
-struct WalkTo<'a, 'b, T> {
-    curr: &'a Node<T>,
-    shared: &'b [u8],
-    rest: &'b [u8],
-}
+    pub fn get_mut<'a>(&'a mut self, key: &[u8]) -> Option<&'a mut T> {
+        let WalkTo {
+            shared, rest, curr, ..
+        } = self.walk_to(key);
 
-struct WalkToMut<'a, 'b, T> {
-    curr: &'a mut Node<T>,
-    shared: &'b [u8],
-    rest: &'b [u8],
+        if rest.is_empty() && shared.len() == curr.prefix.len() {
+            let ix = curr.val?;
+            Some(ix.get_mut(&mut self.item_slab))
+        } else {
+            None
+        }
+    }
+
+    pub fn insert(&mut self, key: &[u8], val: T) -> Option<T> {
+        let walked = self.walk_to(key);
+        let last_ix = walked.last_key();
+        let WalkTo {
+            shared, rest, curr, ..
+        } = walked;
+
+        if shared.len() == curr.prefix.len() {
+            // no need to split curr
+            if let Some((d, suff)) = rest.split_first() {
+                debug_assert!(curr.slots[*d as usize].is_none());
+
+                // first we'll allocate the new item
+                let vix = ItemKey::insert(&mut self.item_slab, val);
+
+                // next we'll make a new node
+                let mut new_node = Node::new();
+                new_node.val = Some(vix);
+                new_node.prefix = suff.into();
+
+                // next we allocate it
+                let nix = NodeKey::insert(&mut self.node_slab, new_node);
+
+                // get a mutable ref to current node
+                let node_ref = if let Some(ix) = last_ix {
+                    ix.get_mut(&mut self.node_slab)
+                } else {
+                    &mut self.root
+                };
+
+                // and place the ix
+                let old_nix = node_ref.slots[*d as usize].replace(nix);
+
+                // if somehow old_nix is not none, we should at least delete the item associated with it
+                debug_assert!(old_nix.is_none());
+
+                None
+            } else if let Some(ix) = curr.val {
+                // if we already have a slot we can reuse it
+                Some(std::mem::replace(ix.get_mut(&mut self.item_slab), val))
+            } else {
+                // get a mutable ref to current node
+                let node_ref = if let Some(ix) = last_ix {
+                    ix.get_mut(&mut self.node_slab)
+                } else {
+                    &mut self.root
+                };
+
+                // then allocate the new item
+                let vix = ItemKey::insert(&mut self.item_slab, val);
+
+                // and place it
+                let old_vix = node_ref.val.replace(vix);
+
+                // if somehow old_vix is not none, we should at least delete the item associated with it
+                debug_assert!(old_vix.is_none());
+
+                None
+            }
+        } else {
+            // we need to split curr
+            assert!(shared.len() < curr.prefix.len());
+            let (old_dig, old_suff) = curr.prefix[shared.len()..].split_first().unwrap();
+            let old_dig = *old_dig;
+
+            let mut new_old = curr.clone();
+            new_old.prefix = old_suff.into();
+            let old_ix = NodeKey::insert(&mut self.node_slab, new_old);
+
+            // allocate the new item
+            let vix = ItemKey::insert(&mut self.item_slab, val);
+
+            let mut new_curr = Node::new();
+            new_curr.prefix = shared.into();
+            new_curr.slots[old_dig as usize].replace(old_ix);
+
+            if let Some((new_dig, new_suff)) = rest.split_first() {
+                debug_assert!(*new_dig != old_dig);
+
+                let mut new_node = Node::new();
+                new_node.prefix = new_suff.into();
+                new_node.val = Some(vix);
+
+                // place the new node
+                let new_ix = NodeKey::insert(&mut self.node_slab, new_node);
+
+                new_curr.slots[*new_dig as usize].replace(new_ix);
+            } else {
+                new_curr.val = Some(vix);
+            }
+
+            // get a mutable ref to current node
+            let node_ref = if let Some(ix) = last_ix {
+                ix.get_mut(&mut self.node_slab)
+            } else {
+                &mut self.root
+            };
+
+            *node_ref = new_curr;
+
+            None
+        }
+    }
 }
 
 fn shared_len<T: Eq>(a: &[T], b: &[T]) -> usize {
